58f00b19455be3521aca0223f90643aa59502ae9
safari <safari@edsab.co>
2020-06-10 16:41:18 +0430

    add jquery types to devDeps and delete unused files

--- TypescriptCoreMvc/Scripts/app.ts
+++ TypescriptCoreMvc/Scripts/app.ts
@@ -1,1 +1,10 @@
-﻿console.log('Hello');
+﻿var jqtest = {
+    showMsg: function (): void {
+        let v: any = jQuery.fn.jquery.toString();
+        let content: any = $("#ts-example-2")[0].innerHTML;
+        //alert(content.toString());
+        $("#ts-example-2")[0].innerHTML = content + " " + v + "!!";
+    }
+};
+
+jqtest.showMsg();
