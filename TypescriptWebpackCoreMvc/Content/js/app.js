247d88f6ec6ab9fa958f4ff84ebf55dc3390314e
safari <safari@edsab.co>
2020-06-28 16:23:57 +0430

    async function and  await import

--- TypescriptWebpackCoreMvc/Content/js/app.js
+++ TypescriptWebpackCoreMvc/Content/js/app.js
@@ -7,9 +7,10 @@ document.getElementById("fillthis").innerHTML = getText();
 let myES6Object = new ES6Lib();
 document.getElementById("fillthiswithes6lib").innerHTML = myES6Object.getData();
 
-import _ from 'lodash';
 import '../css/style.css';
 
+/* sync:
+import _ from 'lodash';
 function component() {
     const element = document.createElement('div');
     // Lodash, currently included via a script, is required for this line to work
@@ -19,6 +20,21 @@ function component() {
 }
 
 document.body.appendChild(component());
+*/
+
+// async:
+async function getComponent() {
+    const element = document.createElement('div');
+    const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    return element;
+}
+
+getComponent().then(component => {
+    document.body.appendChild(component);
+});
 
 /*
  In this setup, index.js explicitly requires lodash to be present, and binds it as _ (no global scope pollution).
