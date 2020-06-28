3d8ac2e07696e42c9e94b2fc46ed33dba079a422
safari <safari@edsab.co>
2020-06-28 09:35:08 +0430

    add lodash and use it

--- TypescriptWebpackCoreMvc/Content/js/app.js
+++ TypescriptWebpackCoreMvc/Content/js/app.js
@@ -5,5 +5,14 @@ import ES6Lib from './es6codelib';
 document.getElementById("fillthis").innerHTML = getText();
 
 let myES6Object = new ES6Lib();
-document.getElementById("fillthiswithes6lib").innerHTML= myES6Object.getData();
+document.getElementById("fillthiswithes6lib").innerHTML = myES6Object.getData();
 
+import _ from 'lodash';
+function component() {
+    const element = document.createElement('div');
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
+}
+
+document.body.appendChild(component());
